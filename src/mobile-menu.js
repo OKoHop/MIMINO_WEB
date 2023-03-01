29105d1b39949881dc9773a256cf404d28f49175
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-03-01 22:06:42 +0200

    fix js

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -15,4 +15,14 @@
       refs.icon_burger.classList.toggle("hidden-icon");
       refs.icon_cross.classList.toggle("hidden-icon");
     }
+
+          // Close the mobile menu on wider screens if the device orientation changes
+          window.matchMedia('(min-width: 1440px)').addEventListener('change', e => {
+            if (!e.matches) { return; }
+            refs.menu.classList.add('is-hidden');
+            refs.icon_cross.classList.add('hidden-icon');
+            refs.icon_burger.classList.remove("hidden-icon");
+            
+          });
   })();
+
