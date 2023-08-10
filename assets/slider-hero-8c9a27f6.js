5a465a380bf65442acd0bca6c70cb8969c5aa14d
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-08-10 16:56:45 +0000

    Deploying to gh-pages from @ OKoHop/project-team-9@36dfe121b698d3952fc64a55d090060b235e5b95 🚀

---
+++ assets/slider-hero-8c9a27f6.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".hero-hotel-gallery").slick({slide:".hero-hotel-gallery-item",slidesToShow:1,dots:!1,slidesToScroll:1,variableWidth:!0,arrows:!0,prevArrow:$(".hero-hotel-left-btn"),nextArrow:$(".hero-hotel-right-btn")})});
