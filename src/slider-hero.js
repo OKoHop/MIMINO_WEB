d57a636ca04716e04a04da0f7339587975b3adec
Inna Shkilna <shkinai.1995@gmail.com>
2023-08-10 19:53:31 +0300

    update slider.js

--- src/slider-hero.js
+++ src/slider-hero.js
@@ -1,12 +1,10 @@
-$(document).ready(function () {
-  $('.hero-hotel-gallery').slick({
-    slide: '.hero-hotel-gallery-item',
-    slidesToShow: 1,
-    dots: false,
-    slidesToScroll: 1,
-    variableWidth: true,
-    arrows: true,
-    prevArrow: $('.hero-hotel-left-btn'),
-    nextArrow: $('.hero-hotel-right-btn'),
-  });
+$('.hero-hotel-gallery').slick({
+  slide: '.hero-hotel-gallery-item',
+  slidesToShow: 1,
+  dots: false,
+  slidesToScroll: 1,
+  variableWidth: true,
+  arrows: true,
+  prevArrow: $('.hero-hotel-left-btn'),
+  nextArrow: $('.hero-hotel-right-btn'),
 });
